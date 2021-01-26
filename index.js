c24ba98b9a0b260d2e3aeeb0b9fdb9dd2c67e383
David Gold <davidgold@hey.com>
2021-01-25 19:47:24 -0500

    Have cloudinary code, need to place it appropriately

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const cloudinary = require("cloudinary");
 require("dotenv").config();
 const { App } = require("@slack/bolt");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const bot = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -17,9 +24,14 @@ bot.event("app_mention", async ({ event, client }) => {
   try {
     await client.chat.postMessage({
       channel: event.channel,
-      text: `Hey yoo <@${event.user}> you mentioned me`,
+      text: `Ask me to see a doodle, and I'll send you a New Yorker cartoon`,
     });
   } catch (e) {
     console.log(`error responding ${e}`);
   }
+});
+
+// To add into slackbot
+cloudinary.v2.api.resources(function (error, result) {
+  console.log(result, error);
 });
